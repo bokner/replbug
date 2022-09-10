dde4378865e76dd22ee1fff02808ae652b67d345
Boris Okner <boris.okner@gmail.com>
2022-09-10 19:56:31 -0400

    README; renaming of modules

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Replbug
 
-**Utilities on top of [Rexbug] (https://github.com/nietaki/rexbug) to facilitate REPL-style debugging in IEx shell.**
+**Utilities on top of [Rexbug](https://github.com/nietaki/rexbug) to facilitate REPL-style debugging in IEx shell.**
 
 ## Installation
 
@@ -15,7 +15,83 @@ def deps do
 end
 ```
 
-Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
-and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at <https://hexdocs.pm/replbug>.
+## Motivation
+
+The Erlang Trace BIFs allow to trace Erlang code on live systems.
+
+[Rexbug](https://github.com/nietaki/rexbug) provides the interface that allows to display and/or write to external files the trace messages emitted by Erlang VM. In many cases, this would be sufficient for the purposes of debugging the code. However, if the size and/or number of tracing messages is large, it becomes more difficult to make sense of what's going on.
+
+To illustrate the issue, let's add Rexbug dependency to our Phoenix server project:
+```elixir
+defp deps do
+    [
+      ### Our deps...
+      {:replbug, "~> 0.1.0"}
+    ]
+  end
+```
+
+, then start a Phoenix server with the LiveDashboard enabled and look at the trace produced by `Phoenix.LiveView.Plug.call/2`
+
+```elixir
+# Start the tracing for Phoenix.LiveView.Plug.call/2
+Rexbug.start("Phoenix.LiveView.Plug.call/2 :: return", time: 60_000, msgs: 1_000)
+```
+Then hit http://localhost:4000/dashboard or whatever link your LiveDashboard is configured for. 
+
+We'll see a lot of output in your IEx shell, which is pretty hard to discern due to the size and truncation caused by pretty-printing.
+
+Replbug does what Rexbug does, but additionally, we could pull the traces to IEx and materialize it as a variable that we could inspect and experiment on:
+
+```elixir
+# Make sure to close Rexbug session
+Rexbug.stop
+# Start the tracing for Phoenix.LiveView.Plug.call/2 with Replbug
+iex(17)> Replbug.start("Phoenix.LiveView.Plug.call/2 :: return", time: 60_000, msgs: 1_000)
+```
+
+Hit http://localhost:4000/dashboard again.
+
+The same amount of output, as with using Rexbug, but now we can get the traces into Iex and inspect them programmatically:
+
+```elixir
+iex(18)> traces = Replbug.stop
+## `traces` is a map PID -> calls
+iex(17)> Map.keys(traces)
+[#PID<0.544.0>, #PID<0.548.0>]
+## These are the processes that made the calls
+## Get the list of calls across all processes
+iex(19)> calls = Replbug.calls(traces)
+## What calls were traced?
+iex(19)> Map.keys(calls)              
+[{Phoenix.LiveView.Plug, :call, 2}]
+## yeah, that's the one we traced...
+## Get the call records (args, returns etc.)
+iex(21)> call_data = Map.get(calls, {Phoenix.LiveView.Plug, :call, 2})
+## What are the arguments of the first call?
+iex(22)> [arg1, arg2] =  hd(call_data).args
+## We expect arg1 to be a Plug.Conn struct...
+iex(23)> iex(24)> is_struct(arg1, Plug.Conn)
+true
+## ...and we are interested in user-agent value...
+iex(24)> List.keyfind(arg1.req_headers, "user-agent", 0) 
+{"user-agent",
+ "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"}
+## What are durations of the calls?
+iex(25)> durations = Enum.map(calls, & &1.duration)
+[9411, 13954]
+## Let's look at the return of the first call.
+iex(26) > return = call_data.return
+## We expect it to be a Plug.Conn struct as well...
+iex(27) > is_struct(return, Plug.Conn)
+true
+## Finally, check the HTTP status
+iex(28) > return.status
+302
+
+'''
+
+
+
+
 
--- lib/replbug.ex
+++ lib/replbug.ex
@@ -1,18 +1,341 @@
 defmodule Replbug do
   @moduledoc """
-  Documentation for `Replbug`.
+  Utility for pulling the function call traces into your IEx shell for further analysis and experimentation.
+  The code is built on top of Rexbug (https://github.com/nietaki/rexbug).
+  Motivation: Rexbug provides a convenient way of tracing function calls
+  by printing the trace messages to IEx shell and/or the external file.
+  In addition, Replbug allows to materialize traces as a variable, and then analyze the call data
+  in IEx for debugging, experimentation, collecting stats etc.
+  Example 1:
+   Trace DateTime.utc_now/0. Let the tracer run for 60 secs or until 10 trace messages are emitted.
+    iex(1)> Replbug.start("DateTime.utc_now/0", time: 60_000, msgs: 10)
+    You could either wait for the system to make DateTime.utc_now/0 calls, or call it manually in IEx.
+    At any time, stop collecting traces and get the call trace records:
+    iex(3)> call_traces = Replbug.stop
+    %{
+    #PID<0.1392.0> => [
+      %{
+        args: [],
+        call_timestamp: ~T[13:58:20.183042],
+        caller_pid: #PID<0.1392.0>,
+        function: :utc_now,
+        module: DateTime,
+        parent: Verk.ScheduleManager,
+        return: ~U[2022-04-11 17:58:20.183039Z],
+        return_timestamp: ~T[13:58:20.183054]
+      },
+      %{
+        args: [],
+        call_timestamp: ~T[13:58:20.184928],
+        caller_pid: #PID<0.1392.0>,
+        function: :utc_now,
+        module: DateTime,
+        parent: Verk.ScheduleManager,
+        return: ~U[2022-04-11 17:58:20.184925Z],
+        return_timestamp: ~T[13:58:20.184937]
+      }
+    ],
+    #PID<0.1498.0> => [
+      %{
+        args: [],
+        call_timestamp: ~T[13:58:18.183879],
+        caller_pid: #PID<0.1498.0>,
+        function: :utc_now,
+        module: DateTime,
+        parent: %Rexbug.Printing.MFA{a: 1, f: :init, m: Oban.Plugins.Stager},
+        return: ~U[2022-04-11 17:58:18.183883Z],
+        return_timestamp: ~T[13:58:18.183899]
+      },
+      %{
+        args: [],
+        call_timestamp: ~T[13:58:19.197818],
+        caller_pid: #PID<0.1498.0>,
+        function: :utc_now,
+        module: DateTime,
+        parent: %Rexbug.Printing.MFA{a: 1, f: :init, m: Oban.Plugins.Stager},
+        return: ~U[2022-04-11 17:58:19.197821Z],
+        return_timestamp: ~T[13:58:19.197836]
+      }
+    ],
+    #PID<0.1538.0> => [
+      %{
+        args: [],
+        call_timestamp: ~T[13:58:17.925199],
+        caller_pid: #PID<0.1538.0>,
+        function: :utc_now,
+        module: DateTime,
+        parent: %Rexbug.Printing.MFA{a: 1, f: :init, m: Oban.Queue.Producer},
+        return: ~U[2022-04-11 17:58:17.925201Z],
+        return_timestamp: ~T[13:58:17.925216]
+      }
+    ]
+  }
+  This tells us that calls to DateTime.utc_now/0 were made by 3 processes:
+  <0.1392.0> (Verk.ScheduleManager, 2 times), <0.1498.0> (Oban.Plugins.Stager, 2 times) and
+  <0.1538.0> (Oban.Queue.Producer, once)
+  Example 2: (get the call durations for the given MFA):
+  iex(1)> Replbug.start("DateTime", "String.contains?/2"], time: 60_000, msgs: 100)
+  ### ...do some function calls with DateTime and/or String.contains?
+  iex(2)> Enum.each(1..4, fn _ -> DateTime.utc_now() end)
+  .......
+  iex(3)> String.contains?("aaa", "b")
+  ### retrieve the call traces
+  iex(4)> calls = Replbug.stop |> Replbug.calls()
+  ### Get the list of calls:
+  iex(5)> Map.keys(calls)
+  [
+  {DateTime, :__info__, 1},
+  {DateTime, :convert, 2},
+  {DateTime, :from_unix, 3},
+  {DateTime, :from_unix!, 3},
+  {DateTime, :utc_now, 0},
+  {DateTime, :utc_now, 1},
+  {String, :contains?, 2}
+  ]
+  ### Get the call durations for DataTime.utc_now/0 :
+  iex(6)> calls |> Map.get({DateTime, :utc_now, 0}) |> Enum.map(& &1.duration)
+  [35, 14, 12, 12]
+  ### Replay calls for DateTime.utc_now/0 :
+  iex(7)> calls |> Map.get({DateTime, :utc_now, 0}) |> Enum.map(&Replbug.replay/1)
+  [~U[2022-08-31 21:02:02.397846Z], ~U[2022-08-31 21:02:02.397857Z],
+   ~U[2022-08-31 21:02:02.397859Z], ~U[2022-08-31 21:02:02.397860Z]]
   """
+
+  alias Replbug.Server, as: CollectorServer
+
+  @trace_collector :trace_collector
+
+  def start(call_pattern, opts \\ []) do
+    # Get preconfigured print_fun (either default one, or specified by the caller)
+    preconfigured_print_fun =
+      Keyword.get(opts, :print_fun, fn t -> Rexbug.Printing.print_with_opts(t, opts) end)
+
+    print_fun = fn trace_record ->
+      ## Call preconfigured print_fun, if any
+      preconfigured_print_fun && preconfigured_print_fun.(trace_record)
+      pid = Process.whereis(@trace_collector)
+      pid && send(pid, {:trace, parse_call_trace(trace_record)})
+    end
+
+    call_pattern
+    |> add_return_opt()
+    |> create_call_collector(Keyword.put(opts, :print_fun, print_fun))
+  end
+
+  def stop do
+    Rexbug.stop()
+
+    Process.whereis(@trace_collector) &&
+      GenServer.call(@trace_collector, :get_trace_data)
+  end
+
+  ## Get PIDs of all processes that made calls traced by Rexbug.
+  def get_caller_pids(trace) do
+    Map.keys(trace)
+  end
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Replbug.hello()
-      :world
-
+  Group the trace by function calls (MFA).
   """
-  def hello do
-    :world
+
+  def calls(trace) do
+    trace
+    |> Map.values()
+    |> List.flatten()
+    |> Enum.group_by(fn trace_rec ->
+      {trace_rec.module, trace_rec.function, length(trace_rec.args)}
+    end)
+  end
+
+  def replay(%{function: f, module: m, args: a} = _call_record) do
+    apply(m, f, a)
+  end
+
+  defp create_call_collector(call_pattern, rexbug_opts) do
+    GenServer.start(CollectorServer, [call_pattern, rexbug_opts], name: @trace_collector)
+  end
+
+  defp add_return_opt(trace_pattern) when trace_pattern in [:send, :receive] do
+    throw({:error, :tracing_messages_not_supported})
+  end
+
+  defp add_return_opt(call_pattern) when is_binary(call_pattern) do
+    ## Force `return` option
+    case String.split(call_pattern, ~r{::}, trim: true, include_captures: true) do
+      [no_opts_call] ->
+        "#{no_opts_call} :: return"
+
+      [call, "::", opts] ->
+        (String.contains?(opts, "return") && call_pattern) ||
+          "#{call} :: return,#{String.trim(opts)}"
+    end
+  end
+
+  defp add_return_opt(call_pattern_list) when is_list(call_pattern_list) do
+    Enum.map(call_pattern_list, fn pattern -> add_return_opt(pattern) end)
+  end
+
+  defp parse_call_trace(trace_record) do
+    trace_record
+    |> Rexbug.Printing.from_erl()
+    |> extract_trace_data()
+  end
+
+  defp extract_trace_data(
+         %Rexbug.Printing.Call{
+           mfa: mfa,
+           from_pid: caller_pid,
+           from_mfa: parent,
+           time: rexbug_time
+         } = trace_rec
+       ) do
+    %{
+      trace_kind: :call,
+      module: mfa.m,
+      function: mfa.f,
+      args: mfa.a,
+      parent: parent,
+      caller_pid: caller_pid,
+      call_timestamp: to_time(rexbug_time),
+      stack: trace_rec.dump && Rexbug.Printing.extract_stack(trace_rec.dump)
+    }
+  end
+
+  defp extract_trace_data(%Rexbug.Printing.Return{
+         mfa: mfa,
+         return_value: return_value,
+         from_pid: caller_pid,
+         time: rexbug_time
+       }) do
+    %{
+      trace_kind: :return,
+      module: mfa.m,
+      function: mfa.f,
+      arity: mfa.a,
+      return: return_value,
+      caller_pid: caller_pid,
+      return_timestamp: to_time(rexbug_time)
+    }
+  end
+
+  defp to_time(%Rexbug.Printing.Timestamp{hours: h, minutes: m, seconds: s, us: us}) do
+    Time.new!(h, m, s, us)
+  end
+end
+
+defmodule Replbug.Server do
+  @behaviour GenServer
+
+  require Logger
+  # Callbacks
+  @impl true
+  def init([call_pattern, rexbug_opts] = _args) do
+    Process.flag(:trap_exit, true)
+
+    case start_rexbug(call_pattern, rexbug_opts) do
+      :ok ->
+        :erlang.monitor(:process, Process.whereis(:redbug))
+        ## The state is a map of {process_pid, call_traces},
+        {:ok, %{traces: Map.new()}}
+
+      error ->
+        {:stop, error}
+    end
+  end
+
+  @impl true
+  def handle_call(:get_trace_data, _from, state) do
+    {:stop, :normal, calls_by_pid(state.traces), Map.put(state, :traces, Map.new())}
+  end
+
+  def handle_call(unknown_message, _from, state) do
+    {:reply, {:unknown_message, unknown_message}, state}
+  end
+
+  @impl true
+  def handle_cast(_cast_msg, state) do
+    {:noreply, state}
+  end
+
+  @impl true
+  def handle_info({:trace, trace_msg}, state) do
+    {:noreply, store_trace_message(trace_msg, state)}
+  end
+
+  def handle_info({:DOWN, _ref, :process, _pid, _reason}, state) do
+    Logger.warn('''
+    The tracing has been completed. Use:
+      Replbug.stop to get the trace records into your shell.
+    ''')
+
+    {:noreply, state}
+  end
+
+  defp start_rexbug(call_pattern, opts) do
+    case Rexbug.start(call_pattern, opts) do
+      {proc_count, func_count} when is_integer(proc_count) and is_integer(func_count) ->
+        :ok
+
+      error ->
+        error
+    end
+  end
+
+  defp calls_by_pid(traces) do
+    Map.new(
+      for {pid, {finished_calls, unfinished_calls}} <- traces do
+        case length(unfinished_calls) do
+          unfinished_count when unfinished_count > 0 ->
+            Logger.warn("""
+            There are #{unfinished_count} unfinished calls in the trace.
+            Some traced calls may still be in progress, and/or the number of trace messages has exceeded the value for :msgs option.
+            """)
+
+          _ ->
+            :ok
+        end
+
+        {pid, Enum.reverse(finished_calls)}
+      end
+    )
+  end
+
+  defp store_trace_message(
+         %{trace_kind: :call, caller_pid: caller_pid} = call_trace_msg,
+         %{traces: traces} = state
+       ) do
+    Map.put(
+      state,
+      :traces,
+      Map.update(traces, caller_pid, {[], [call_trace_msg]}, fn {finished_calls, unfinished_calls} ->
+        {finished_calls, [call_trace_msg | unfinished_calls]}
+      end)
+    )
+  end
+
+  defp store_trace_message(
+         %{trace_kind: :return, caller_pid: caller_pid} = return_trace_msg,
+         %{traces: traces} = state
+       ) do
+    Map.put(
+      state,
+      :traces,
+      Map.update(traces, caller_pid, {}, fn {finished_calls, [last_call | rest]} ->
+        {
+          [
+            return_trace_msg
+            |> Map.merge(last_call)
+            |> Map.drop([:arity, :trace_kind])
+            |> Map.put(:duration, call_duration(return_trace_msg, last_call))
+            | finished_calls
+          ],
+          rest
+        }
+      end)
+    )
+  end
+
+  defp call_duration(return_rec, call_rec) do
+    Time.diff(return_rec.return_timestamp, call_rec.call_timestamp, :microsecond)
   end
 end
