1b03e6c8f912a50c189c71620dad59cf456c7da4
Boris Okner <boris.okner@gmail.com>
2022-09-10 20:04:59 -0400

    Changes to mix.exs

--- mix.exs
+++ mix.exs
@@ -7,10 +7,27 @@ defmodule Replbug.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
+  defp package do
+    [
+      licenses: ["MIT"],
+      maintainers: ["Boris Okner <boris.okner@gmail.com>"],
+      links: %{
+        "GitHub" => "https://github.com/bokner/replbug"
+      },
+      description: description()
+    ]
+  end
+
+  defp description() do
+      """
+      Replbug is an addition to Rexbug that allows to inspect Erlang VM traces as variables in IEx
+      """
+  end
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
@@ -21,6 +38,7 @@ defmodule Replbug.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:rexbug, "~> 1.0"}
     ]
   end
