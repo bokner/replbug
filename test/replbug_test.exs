235629ee0d9eaaa8a54ed07f616a40d0578606ee
Boris Okner <boris.okner@gmail.com>
2022-09-11 13:19:02 -0400

    Add test for :send, :receive specs not breaking Rexbug functionality

--- test/replbug_test.exs
+++ test/replbug_test.exs
@@ -1,5 +1,9 @@
 defmodule ReplbugTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: false
+
+  setup do
+    on_exit(fn -> Replbug.stop end)
+  end
 
   test "Replbug functionality" do
     timeout = 500
@@ -16,7 +20,7 @@ defmodule ReplbugTest do
 
     call_recs = Map.get(calls, {MapSet, :new, 1})
     ## Check if all calls are in the trace
-    assert num_calls = length(call_recs)
+    assert num_calls == length(call_recs)
     ## Check the call args
     assert Enum.all?(call_recs, fn rec -> rec.args == [call_arg] end)
     ## Check the call returns
@@ -24,4 +28,17 @@ defmodule ReplbugTest do
     ## Replay the calls (use with caution in prod!)
     assert Enum.all?(call_recs, fn rec -> rec.return == Replbug.replay(rec) end)
   end
+
+  test "Allows :send and :receive in trace specs" do
+    timeout = 500
+    num_calls = 3
+    my_pid = self()
+    {:ok, _collector_pid} = Replbug.start(["DateTime.utc_now/0", :send, :receive], procs: [my_pid], time: timeout, msgs: 10000)
+    send my_pid, :test_msg
+    _returns = Enum.map(1..num_calls, fn _i -> DateTime.utc_now end)
+    :timer.sleep(2 * timeout)
+    calls = Replbug.stop() |> Replbug.calls()
+    assert length(Map.get(calls, {DateTime, :utc_now, 0})) == num_calls
+  end
+
 end
